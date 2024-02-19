0909ec411688167561433fcaff8c8eaa44525f91
OmarENNEDDI <enneddiomar@gmail.com>
2024-02-19 22:48:34 +0100

    Ajout des Factories et des Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,6 +28,8 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'description' => fake()->paragraph(),
+            'phone' =>fake()->phoneNumber(),
             'remember_token' => Str::random(10),
         ];
     }
