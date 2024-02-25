740b2cf2cf3b1e7f9019903959673a75972ab558
OmarENNEDDI <enneddiomar@gmail.com>
2024-02-25 15:35:23 +0100

    update model offer

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Offer extends Model
+class Offer extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory , InteractsWithMedia;
 
     protected $fillable = [
         'user_id',
@@ -35,7 +37,6 @@ class Offer extends Model
     {
         return $this->hasMany(Application::class);
     }
-
     public function skills(){
         return $this->belongsToMany(Skill::class);
     }
