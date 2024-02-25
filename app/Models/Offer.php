b6a5d4acd94949e99f056f662f6ec98774f088b5
OmarENNEDDI <enneddiomar@gmail.com>
2024-02-25 15:33:13 +0100

    update new conflicts

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -36,5 +36,7 @@ class Offer extends Model
         return $this->hasMany(Application::class);
     }
 
-    
+    public function skills(){
+        return $this->belongsToMany(Skill::class);
+    }
 }
